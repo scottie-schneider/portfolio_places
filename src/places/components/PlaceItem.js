3ad72cc95eb7a82ca1fa99836d3fb906ebc7faab
scottie-schneider <scott@laclave.co>
2020-01-10 16:08:04 -0500

    completing map modal

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
 import Modal from '../../shared/components/UIElements/Modal'
+import Map from '../../shared/components/UIElements/Map';
+
 import './PlaceItem.css';
 
 const PlaceItem = props => {
@@ -20,7 +22,10 @@ const PlaceItem = props => {
         footer={<Button onClick={closeMapHandler}>Close</Button>}
       >
         <div className="map-container">
-          <h2>The map!</h2>
+          <Map 
+            center={props.coordinates}
+            zoom={16}
+          />
         </div>
       </Modal>
       <li className="place-item">
